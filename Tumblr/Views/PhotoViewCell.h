7a28d795cec493909ea8177fb71782523c4eaddc
Anvitha <anvithak23733@gmail.com>
2021-06-24 09:49:02 -0700

    Finished the lab!

---
+++ Tumblr/Views/PhotoViewCell.h
@@ -0,0 +1,17 @@
+//
+//  PhotoViewCell.h
+//  Tumblr
+//
+//  Created by Anvitha Kachinthaya on 6/24/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotoViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *photoView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tumblr/Views/PhotoViewCell.m
@@ -0,0 +1,23 @@
+//
+//  PhotoViewCell.m
+//  Tumblr
+//
+//  Created by Anvitha Kachinthaya on 6/24/21.
+//
+
+#import "PhotoViewCell.h"
+
+@implementation PhotoViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
